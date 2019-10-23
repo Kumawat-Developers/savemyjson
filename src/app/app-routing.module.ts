e5dff40decb6b3c58836786b581e5824c0e6e605
Kamleshkns <kamleshknskumawat@gmail.com>
2019-10-23 23:23:55 +0530

    fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,24 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MyjsonComponent } from './myjson/myjson.component';
 import { NewjsonComponent } from './newjson/newjson.component';
+
 const routes: Routes = [  
+  
   {
     path:'newjson',
     component:NewjsonComponent
   },
+  
   {
     path:'',
     component:NewjsonComponent
   },
+ 
   {
     path:'myjson/:id',
     component:MyjsonComponent
   }
+  
 ];
 
 @NgModule({
